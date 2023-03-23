0d10ef49c174c2d6285a3c62fd7b5862c7ff321f
MelihEmreGuler <melihemreguler@hotmail.com>
2023-03-23 16:04:30 +0300

    week 2 circular list Node completed

---
+++ src/week2/circularListExample/Node.java
@@ -0,0 +1,11 @@
+package week2.circularListExample;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int dataParameter){
+        this.data = dataParameter;
+        next = null;
+    }
+}
